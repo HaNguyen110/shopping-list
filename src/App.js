cde0c73868dd1d5906ca183c488624573d1d8397
HaNguyen <hanguyen091130@gmail.com>
2021-01-04 22:07:28 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+import { HashRouter, Route } from "react-router-dom";
+import Share from "./Share";
+import ShoppingList from "./shoppingList";
+function App() {
+  return(
+    <div className="App">
+      <HashRouter>
+           <Route path="/" exact component={ShoppingList} />
+          <Route path="/share" component={Share} />
+
+      </HashRouter>
+    </div>
+  )
+};
+
+export default App;
